c6213602561ab0d86a330b26c724efc3e0023911
BreadGhoti1 <148354667+BreadGhoti1@users.noreply.github.com>
2024-01-02 20:18:07 -0600

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -18,5 +18,6 @@ if jq ".enablekvm" "$json_file" | grep -q true; then
 else
     docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
 fi
+docker run --rm -d -p 3000:3000/tcp -p 3001:3001/tcp desktoponcodespaces:latest
 clear
 echo "INSTALL FINISHED! Download Docker Extension and run image then open port 3000 in a new tab!!!"
